da42579df67864cec4d57ea7716e68f3e622a427
Yden Da <ydenda@Ydens-MacBook-Pro.local>
2023-03-20 16:13:46 -0400

    Added user authentication and page restrictions

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import TaskDetail, TaskList, TaskCreate, TaskUpdate, DeleteView
+from .views import TaskDetail, TaskList, TaskCreate, TaskUpdate, DeleteView, CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('create-task/', TaskCreate.as_view(), name='task-create'),
