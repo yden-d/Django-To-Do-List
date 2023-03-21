e632f3b9130b61fd62969d8f0d4e48d3fac6b0d9
Yden Da <ydenda@Ydens-MacBook-Pro.local>
2023-03-20 20:28:05 -0400

    Added user registration

--- base/views.py
+++ base/views.py
@@ -2,12 +2,13 @@ from asyncio import tasks
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
-
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 from .models import Task
 # Create your views here.
@@ -20,6 +21,17 @@ class CustomLoginView(LoginView):
     def get_success_url(self) -> str:
         return reverse_lazy('tasks')
 
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
 
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
